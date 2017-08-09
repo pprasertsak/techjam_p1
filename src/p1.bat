6f71992ac5e3e67a1fda0f35637d0fb06a674d61
Prasertsak <prasertsak@gmail.com>
2017-08-09 18:41:25 +0700

    Create Jupyter version

--- src/p1.bat
+++ src/p1.bat
@@ -1,1 +1,1 @@
-python p1.py ..\data\trx.csv ..\data\card.csv ..\data\cust.csv ..\data\train.csv ..\data\test.csv
+python p1.py ..\data\trx.csv ..\data\card.csv ..\data\cust.csv ..\data\train.csv ..\data\test.csv > test_results.txt
